754d7ca992da516564a787b9c6cc4920aa24af47
a22martinbb <a22martinbb@iessanclemente.net>
2023-03-17 11:45:37 +0100

    s

---
+++ app/entrypoint-dev.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/usr/sbin/php-fpm8.2
+
+
+
+symfony serve --dir /app/symfony/my_project
+
---
+++ app/entrypoint-nginx.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/usr/sbin/php-fpm8.2 -F
+
+#symfony serve --dir /app/symfony/my_project
